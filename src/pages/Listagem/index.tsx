d5daf7878d72cd888737630673bffef1cbcfa2b8
Reinaldo Silveira <reinaldopsilveira@gmail.com>
2022-06-13 22:55:49 -0300

    paginação na listagem

--- src/pages/Listagem/index.tsx
+++ src/pages/Listagem/index.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import style, { Container, Title, TitleBold } from "./styles";
 import { useNavigation } from '@react-navigation/native';
 import { Header } from "../../components/Header";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 import { ButtonCard } from "../../components/ButtonCard";
 import { BaseContainer } from "../../components/BaseContainer";
 import { api } from "../../api";
 import { useToast } from "native-base";
 import { ToastLayout } from "../../components/ToastLayout";
 import { useCarrinhoStore } from "../../store/carrinho.store";
+import { PRIMARY } from "../../styles/colors";
 
 interface ItemsProps{
     id: number;
@@ -22,7 +23,11 @@ interface ItemsProps{
 export const Listagem: React.FC = () => {
 
     const [active, setActive] = useState<number>();
+    const [page, setPage] = useState<number>(1);
+    const [refreshing, setRefreshing] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
     const [list, setList] = useState<ItemsProps[]>([]);
+    const [last, setLast] = useState<boolean>(false);
     const toast = useToast();
     const navigation = useNavigation<any>();
     const addItem = useCarrinhoStore(state => state.addItem);
@@ -35,18 +40,50 @@ export const Listagem: React.FC = () => {
         })
     }
 
-    const getData = async () => {
-        try {
-            const response = await api.get('games');
-            setList(response.data);
-        } catch(e: any) {
-            toast.show({
-                placement: "top-right",
-                render: ({id}) => {
-                    return ToastLayout.error({id, description: e.message, close: toast.close});
+    const getData = async (pageNumber: number = 1) => {
+        if (!last) {
+            try {
+                setPage(pageNumber);
+                const response = await api.get('games', {
+                    params: {
+                        _page: pageNumber,
+                        _limit: 2,
+                    }
+                });
+                if (pageNumber == 1) {
+                    setList(response.data);    
+                } else {
+                    setList(atual => [...atual, ...response.data]);
                 }
-            })
+
+                if (response.data.length == 0) {
+                    setLast(true);
+                }
+                
+            } catch(e: any) {
+                toast.show({
+                    placement: "top-right",
+                    render: ({id}) => {
+                        return ToastLayout.error({id, description: e.message, close: toast.close});
+                    }
+                })
+            }
         }
+    }
+
+    const updateDate = async () => {
+        if (!last) {
+            setLoading(true);
+            await getData(page +1);
+            setLoading(false);
+        }
+    }
+
+    const reset = async () => {
+        setRefreshing(true);
+        setLast(false);
+        await getData();
+        setRefreshing(false);
     }
 
     useEffect(() => {
@@ -75,6 +112,16 @@ export const Listagem: React.FC = () => {
                     style={style.flatList}
                     numColumns={2}
                     data={list}
+                    refreshing={refreshing}
+                    onRefresh={reset}
+                    onEndReached={updateDate}
+                    ListFooterComponent={() => {
+                        if (loading) {
+                            return (<ActivityIndicator size={'large'} color={PRIMARY} />)
+                        } else {
+                            return null;
+                        }
+                    }}
                 />
             </Container>
         </BaseContainer>
